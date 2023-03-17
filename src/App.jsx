2cddf73ddbb1b333b061b5be1607f508bd4e0c7d
Atharva Desai <atharvadesai@Atharvas-MacBook-Air.local>
2023-03-17 22:18:43 +0530

    Added Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -137,6 +137,7 @@ function App() {
               )
             ) : (
               <div
+              
                 style={{
                   height: "100vh",
                   width: "100vw",
@@ -145,6 +146,22 @@ function App() {
                   justifyContent: "space-around",
                 }}
               >
+
+<nav class="navbar navbar-expand-lg bg-body-tertiary fixed-top">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">MedVault</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">Home</a>
+        </li>
+     </ul>
+    </div>
+  </div>
+</nav>
                 <motion.div
                   style={{
                     display: "flex",
